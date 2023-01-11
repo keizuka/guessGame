a760e4af519c5b8b376d29d2932b94996fd4282a
keizuka <benjamin.ardilouze212@gmail.com>
2023-01-11 14:02:17 +0100

    v2
    new gui seting

--- app.py
+++ app.py
@@ -1,32 +1,20 @@
-
+import tkinter
 from tkinter import *
+app = tkinter.Tk()
+app.title("The guessing game")
+app.maxsize(600, 400)
+app.minsize(600, 400)
+app.iconbitmap("logo.ico")
+app.config(background='#939393')
 
-# premiere fenetre
-rootWindow = Tk()
-nextWindow = Tk()
-# perso
-rootWindow.title("guessing game")  # nom de la fenetre
-rootWindow.geometry("400x600")  # taille de la fenetre
-rootWindow.iconbitmap("logo.ico") # logo de l'app
-rootWindow.config(background='#939393')  # couleur de fond
-rootFrame = Frame(rootWindow, bg='#939393', bd=1,)
-nextWindow.title("starting")
-nextWindow.geometry("400x600")
-nextWindow.iconbitmap("logo.ico")
-nextWindow.config(background='#939393')
-# ajout premier text
-label_title = Label(rootWindow, text="welcome to the wheel of price", bg='#939393', fg='black', font=("Courrier", 18))  # bg=background fg=frontground
-label_title.pack(pady=50)
-label_subtitle = Label(rootWindow, text="just a simple rule don't be a dick", bg='#939393', fg='white', font=("Courrier", 10))
-label_subtitle.pack()
-# ajout du premier bouton
-sartBouton = Button(rootWindow, text="Start", bg='white', fg='black', font=("Courrier", 18), command=exit, COMMAND=nextWindow)
-sartBouton.pack(pady=25, fill=X, side=BOTTOM)
-width = 300
-height = 300
-image = PhotoImage(file="a4e_cockpit_photo.png").zoom(10).subsample(32)
-canvas = Canvas(rootWindow, width=width, height=height, bg='#939393', bd=0, highlightthickness=0)
-canvas.create_image(width/2, height/2, image=image)
-canvas.pack(expand=YES)
-# afficher
-rootWindow.mainloop()
+label_welcome = tkinter.Label(app, text="welcome to the guessing game\n i hope you enjoy it", bg='#939393', fg='black', font=("Courrier", 18))
+label_welcome.pack()
+label_info = tkinter.Label(app, text="please enter your year an mounth birth for verify your age\n thank's\n",)
+entry_year = tkinter.Entry(app, width=4)
+entry_mounth = tkinter.Entry(app, width=2)
+appFrame = Frame(app, entry_mounth)
+#appFrame = Frame(app, entry_year)
+entry_year.pack()
+entry_mounth.pack()
+
+app.mainloop()
---
+++ test.py
@@ -0,0 +1,5 @@
+import tkinter
+app=tkinter.Tk()
+entry_name=tkinter.Entry(app, show="0")
+entry_name.pack()
+app.mainloop()

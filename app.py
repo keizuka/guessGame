e91153a299fc26a5294c2cf0841ad0a59665e4c1
keizuka <benjamin.ardilouze212@gmail.com>
2023-01-12 10:55:12 +0100

    the same

--- app.py
+++ app.py
@@ -4,6 +4,10 @@ import datetime
 import random
 year = datetime.date.year
 month = datetime.date.month
+def age():
+    age:year-birthYear
+def birthMonth():
+    birthMonth==tkinter.Entry(app,)
 app = tkinter.Tk()
 app.title("guessing game")
 app.iconbitmap("logo.ico")
@@ -12,21 +16,29 @@ label_welcome = tkinter.Label(app, text="Welcome to the guessing game", font=18)
 label_welcome.pack()
 label_info = tkinter.Label(app, text="We need some information about you,\n something like your year and month birth\n\n")
 label_info.pack()
-birthYear = tkinter.Entry(width=4) # année de naissance de l'utilisateur
+birthYear = tkinter.Entry (app, width=4) # année de naissance de l'utilisateur
 birthYear.pack()
 sauteLigne = tkinter.Label(app, text=("year"))
 sauteLigne.pack()
-birthMonth = tkinter.Entry(width=2) # mois de naissance de l'utilisateur
+birthMonth(type(int)) == tkinter.Entry(app, width=2) # mois de naissance de l'utilisateur
 birthMonth.pack()
 sauteLigne = tkinter.Label(app, text=("month"))
 sauteLigne.pack()
 sauteLigne = tkinter.Label(app, text=("\n"))
 sauteLigne.pack()
-button_calc = tkinter.Button(app, text="checking", width=7, height=2)
+
+while birthMonth!=month:
+    if birthMonth(type(int))<month:
+        age_label = tkinter.Label (age, text="years old")
+        age_label.pack()
+    else:
+        birthMonth>=month
+        age_label = tkinter.Label (age + 1, text="years old")        
+button_calc = tkinter.Button(app, text="checking", width=7, height=2, command=age)
 button_calc.pack()
 sauteLigne = tkinter.Label(app, text=("\n"))
 sauteLigne.pack()
-age_label = tkinter.Label(app)
+
 
 
 
--- test.py
+++ test.py
@@ -1,5 +1,25 @@
-import tkinter
-app=tkinter.Tk()
-entry_name=tkinter.Entry(app, show="0")
-entry_name.pack()
+from tkinter import *
+import datetime
+import math
+yt = datetime.date.year
+mt = datetime.date.month
+app = Tk()
+app.title("guessing game")
+app.iconbitmap("logo.ico")
+app.minsize(600, 400)
+label_welcome = Label(app, text="Welcome to guessing game\n")
+label_welcome.pack()
+label_welcome2 = Label(app, text="in first we need some information\n")
+label_welcome2.pack()
+year = IntVar
+birthyear = int(input())(app, textvariable=year)
+birthyear.pack()
+age = yt-birthyear
+yearlabel = Label(app, age)
+yearlabel.pack()
+def function():
+    yearlabel['text'] = year.get()
+bouton = Button(app, text="click me!!", command=function)
+bouton.pack()
+
 app.mainloop()
